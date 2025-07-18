b8bc7e5f481e123f798d48865a06323b66caa74d
geni-icfp25-ae <geni.icfp25.ae@gmail.com>
2025-07-18 19:08:40 -0400

    Update run.sh
    detailed time breakdown

--- bench/ICFP/run.sh
+++ bench/ICFP/run.sh
@@ -6,29 +6,31 @@
     time SANITY_CHECK=1 make -C bayesian-networks dice-summary.csv
     echo "make -C bayesian-networks clean"
     make -C bayesian-networks clean
-    
+
+    export SANITY_CHECK=0    
     echo "Running benchmarks..."
     echo "SANITY_CHECK=${SANITY_CHECK}"
 
-    time make -C weather-factory bench.dice
-    time make -C weather-factory bench.gennifer
+    time NUM_SAMPLES=30000 make -C poisson-packet-arrival poisson_packet_arrival_pyro.csv # 15.82 mins
+    time make -C poisson-packet-arrival poisson_packet_arrival_geni.csv
+    time make -C poisson-packet-arrival poisson_packet_arrival_pyro.png
+
+    time make -C weather-factory bench.dice # 70m47.652s
+    time make -C weather-factory bench.gennifer # 34m59.847s
     time make -C weather-factory test_horizon.png
     time make -C weather-factory test_factory.png
 
-    time make -C grid-networks link-failure.genfer.csv
-    time make -C grid-networks link-failure.problog.csv
-    time make -C grid-networks link-failure.dice.csv
-    time make -C grid-networks link-failure.gennifer.csv
+    time make -C grid-networks link-failure.genfer.csv # 20m30.931s
+    time make -C grid-networks link-failure.problog.csv # 7m33.135s
+    time make -C grid-networks link-failure.dice.csv # 161m28.805s
+    time make -C grid-networks link-failure.gennifer.csv # 99m24.736s
     time make -C grid-networks link-failure.png
 
-    time make -C grid-networks router-failure.dice.csv
-    time make -C grid-networks router-failure.gennifer.csv
+    time make -C grid-networks router-failure.dice.csv # 286m7.356s
+    time make -C grid-networks router-failure.gennifer.csv # 117m42.930s
     time make -C grid-networks router-failure.png
 
-    time make -C bayesian-networks dice-summary.csv
-    time make -C bayesian-networks gennifer-l-i--pt-summary.csv
+    time make -C bayesian-networks dice-summary.csv # 68m49.763s
+    time make -C bayesian-networks gennifer-l-i--pt-summary.csv #1030m41.729s
 
-    time NUM_SAMPLES=30000 make -C poisson-packet-arrival poisson_packet_arrival_pyro.csv
-    time make -C poisson-packet-arrival poisson_packet_arrival_pyro.png
-    time make -C poisson-packet-arrival poisson_packet_arrival_geni.csv
 } 2>&1 | tee run.log
